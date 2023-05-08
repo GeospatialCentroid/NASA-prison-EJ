f499a386cd514a5c90250754486bfde0b4188a9c
Caitlin Mothes <ccmothes@gmail.com>
2023-05-08 13:39:32 -0600

    write script to process traffic proximity dataset

--- R/R_scratch/getTrafficDensity.R
+++
@@ -1,9 +0,0 @@
-# working script to calculate traffic density indicator
-
-library(tidyverse)
-library(vroom)
-library(sf)
-
-
-# read in raw data
-traffic <- vroom("data/raw/traffic_density/TMAS_2020.csv")
