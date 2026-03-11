b6f8fc9fb281ff40647c159e0e620fa94c475e32
jackson.phippen@gmail.com <jackson.phippen@gmail.com>
2026-03-11 12:49:30 -0600

    updated to account for npl files to be a csv or shapefile.

--- R/calc_npl_proximity.R
+++ R/calc_npl_proximity.R
@@ -4,7 +4,7 @@
 #' facilities within 5km (or nearest one beyond 5km) each divided by the distance in km.
 #' 
 #' @param sf_obj An sf object of all polygons to be assessed
-#' @param file The filepath to the NPL csv file
+#' @param file The filepath to the NPL geocoded shape file (or csv file type for older implementation)
 #' @param dist The distance (in meters) to count facilities within. Default is 5000 (5km)
 #' @param save Whether to save the resulting dataframe (as .csv) or not.
 #' @param out_path If `save = TRUE`, the file path to save the dataframe.
@@ -16,16 +16,28 @@ calc_npl_proximity <- function(sf_obj,
                      save = TRUE,
                      out_path = "outputs/") {
   
-  npl <- read_csv(file) %>% 
-    #keep only listed and proposed NPL
-    filter(str_detect(npl_status, "Final|Proposed")) %>% 
-    separate(geometry, into = c("Long", "Lat"), sep = ",") %>% 
-    mutate(Long = str_remove(Long, ".*\\("),
-           Lat = str_remove(Lat, "\\)")) %>%
-    filter(!is.na(Long) | !is.na(Lat)) %>% 
-    st_as_sf(coords = c("Long", "Lat"), crs = 4326) %>% 
-    st_transform(crs = st_crs(sf_obj))
+  #check file type
+  extension = file_ext(file)
   
+  if(extension == "csv"){
+    
+    npl <- read_sf(file) %>% 
+      #keep only listed and proposed NPL
+      filter(str_detect(npl_status, "Final|Proposed")) %>% 
+      separate(geometry, into = c("Long", "Lat"), sep = ",") %>%
+      mutate(Long = str_remove(Long, ".*\\("),
+             Lat = str_remove(Lat, "\\)")) %>%
+      filter(!is.na(Long) | !is.na(Lat)) %>% 
+      st_as_sf(coords = c("Long", "Lat"), crs = 4326) %>% 
+      st_transform(crs = st_crs(sf_obj))
+    
+  } else if(extension == "shp"){
+    
+    npl <- read_sf(file) %>% 
+      filter(str_detect(NPL_Status,"Final|Proposed")) %>% 
+      st_transform(crs=st_crs(sf_obj))
+    
+  }
   
   npl_prox <- effects_proximity(sf_obj, npl, dist = dist) %>% 
     rename(npl_prox = proximity_score)
@@ -36,7 +48,4 @@ calc_npl_proximity <- function(sf_obj,
     write_csv(npl_prox, file = paste0(out_path, "/npl_proximity_", Sys.Date(), ".csv"))
     
   }
-  
-  
-  
 }
