92395cdb8498124bea6af1fb5e4a3773d386acb6
Caitlin Mothes <ccmothes@gmail.com>
2026-04-01 14:47:39 -0600

    process new traffic data

--- process_data/process_traffic.R
+++ process_data/process_traffic.R
@@ -1,53 +1,48 @@
-# working script to calculate traffic density indicator
+# Process 2023 Traffic Data
+## Downloaded from: https://geodata.bts.gov/datasets/483bd180fe814872b82a66dbf65e25f0/about 
+# From metadata (https://www.fhwa.dot.gov/policyinformation/hpms/fieldmanual/hpms_field_manual_dec2016.pdf)
+# f_system 1 - Interstate, 2 and 3 - Principal Arterial, 4 - Minor Arterial in urban areas only (NOT urban_code 99999, that is Rural)
 
 library(tidyverse)
 library(sf)
-library(arcpullr)
 
+# Data organized in geodatabase with a layer for each state. 
+## Read in and clean each state, then bind at the end
 
-
-#get string of state names to loop over
-states <- state.name %>% 
-  str_replace_all(" ", "") %>% 
-  # remove Alaska (no access to this layer for some reason) and add District of Columbia
-  subset(., . != "Alaska") %>% 
-  c(., "District")
-
-
-# retrieve data for each state, filter, and bind to create a single dataset
-
-state_traffic <- vector("list", length = length(states))
-
-for (i in 1:length(states)) {
+traffic_all <- map(state.abb, function(x){
   
-  url <- paste0("https://geo.dot.gov/server/rest/services/Hosted/", states[i], "_2018_PR/FeatureServer/0")
+  data <- st_read("data/phase2/raw/Traffic/HPMS2023/HPMS2023.gdb", layer = paste0("HPMS_FULL_", x, "_2023"))
   
+  #some state missing column naming or case is different (CT for example)
+  if("Field6" %in% names(data)) {
+    data <- data %>% 
+      rename(F_SYSTEM = Field6, URBAN_ID = Field8)
+  }
   
-  # read and filter state layer
-  # From metadata (https://www.fhwa.dot.gov/policyinformation/hpms/fieldmanual/hpms_field_manual_dec2016.pdf)
-  # f_system 1 - Interstate, 2 and 3 - Principal Arterial, 4 - Minor Arterial in urban areas only (NOT urban_code 99999, that is Rural)
+  data <- data %>% 
+    # make all columns uppercase
+    rename_with(toupper) %>% 
+    filter(F_SYSTEM %in% 1:3 | F_SYSTEM == 4 & URBAN_ID != 99999)
   
-  state_traffic[[i]] <- get_spatial_layer(url) %>% 
-    filter(f_system %in% 1:3 | f_system == 4 & urban_code != 99999)
-  
-  
-  print(i)
-  
-  
-}
+  return(data)
+               
+})
 
-# in case there was an error, save the list object so far
-
-save(state_traffic, file = "data/processed/traffic_proximity/state_traffic_list.RData")
-
-
-aadt_2018 <- bind_rows(state_traffic)
-
+# post processing needed before binding
+traffic_cleaned <- map(traffic_all, ~mutate(., across(-all_of(attr(., "sf_column")), as.character)))
+  
+aadt_2023 <- bind_rows(traffic_cleaned)
 
 # project to CRS of prisons for analysis
-prisons <- read_sf("data/processed/study_prisons.shp")
+prisons <- read_sf("data/phase2/processed/prisons/study_prisons.gpkg")
 
-aadt_2018 <- st_transform(aadt_2018, st_crs(prisons))
+# check if they match
+st_crs(aadt_2023) == st_crs(prisons)
+# TRUE! Otherwise run the line below:
+#aadt_2023 <- st_transform(aadt_2023, st_crs(prisons))
 
-save(aadt_2018, file = "data/processed/traffic_proximity/aadt_2018.RData")
+# saving as geopackage and RData in case for file size
+write_sf(aadt_2023, "data/phase2/processed/traffic/aadt_2023.gpkg")
+
+save(aadt_2023, file = "data/phase2/processed/traffic/aadt_2023.RData")
 
